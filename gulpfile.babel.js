c582fe84cd813fdffff1501fcc8d44379582425a
Chris Hoage <iam@chrishoage.com>
2015-08-18 17:46:03 -0700

    better dev server task logging

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -155,7 +155,7 @@ gulp.task('dev-server', () => {
     historyApiFallback: true
   }).listen(PORT, HOST, (err) => {
     if (err) throw new gutil.PluginError('webpack', err)
-    gutil.log('[dev]', `Open http://${HOST}:${PORT}/`)
+    gutil.log('[dev-server]', 'listening on', gutil.colors.cyan(`http://${HOST}:${PORT}`))
   })
 })
 
